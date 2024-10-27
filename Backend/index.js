d0a39609a0a54cca357ec1c2849458d69ca2320b
Jitugopale <jiteshgopale26@gmail.com>
2024-10-27 11:02:26 +0530

    Initialize project using Create React App

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+connectToMongo();
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+// Available Routes
+
+app.use('/api/auth',require('./routes/auth'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on port http://localhost:${port}`)
+})
